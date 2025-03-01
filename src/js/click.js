4a5b053fdd2231643b0f20bc4fb549daacba977f
Isaias Lourenço <isaias.lourenco2020@outlook.com>
2025-03-01 06:20:29 -0300

    Acrescentando JavaScript para gerar conselhos automaticamente através de consumo de API

--- src/js/click.js
+++ src/js/click.js
@@ -1,7 +1,31 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const button = document.querySelector(".container .click");
+document.addEventListener("DOMContentLoaded", () => {
+  const adviceUpdateButton = document.querySelector(".click");
+  const adviceNumber = document.querySelector(".adv-number");
+  const adviceDescription = document.querySelector(".adv-desc");
 
-    button.addEventListener("click", function () {
-        this.classList.toggle("active");
-    });
+  async function getAdvice() {
+      try {
+          const response = await fetch("https://api.adviceslip.com/advice");
+
+          if (!response.ok) {
+              throw new Error("Ocorreu um erro ao tentar buscar as informações da API");
+          }
+
+          const adviceContent = await response.json();
+          const adviceId = `Advice #${adviceContent.slip.id}`;
+          const adviceText = `"${adviceContent.slip.advice}"`;
+          adviceNumber.innerText = adviceId;
+          adviceDescription.innerText = adviceText;
+      } catch (error) {
+          console.error("Erro ao tentar buscar as informações da API", error);
+      }
+  }
+
+  if (adviceUpdateButton) {
+      adviceUpdateButton.addEventListener("click", getAdvice);
+  } else {
+      console.error("Botão não encontrado!");
+  }
+
+  getAdvice();
 });
